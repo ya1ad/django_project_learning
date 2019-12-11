54286808e5901b0e0254810b26d8c94e67d86f2f
yasir <aff3ad@gmail.com>
2019-12-11 09:05:15 +0200

    Database Migration, Fetching Posts from DB.

--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 # Create your views here.
 
-posts = [
-    {
-        'author': 'jhon doe',
-        'title': 'Blog Post 1',
-        'content': '1st post',
-        'date_posted': ' August 27, 2018'
-    },
-    {
-        'author': 'doe kkio',
-        'title': 'Blog Post 2',
-        'content': '2nd post',
-        'date_posted': ' August 27, 2018'
-    },
-    {
-        'author': 'agdum bagdum',
-        'title': 'Blog Post 3',
-        'content': '3rd post',
-        'date_posted': ' August 27, 2018'
-    }
-]
+
 
 def home(request):
 
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
