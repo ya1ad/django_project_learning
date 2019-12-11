8d60d5b9bedd3da0770ada1dcf0490a9c5ad85d4
yasir <aff3ad@gmail.com>
2019-12-11 16:16:17 +0200

    Modify of user status

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegistartionForm
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def register(request):
@@ -12,7 +12,10 @@ def register(request):
                 form.save()
                 username = form.cleaned_data.get('username')
                 messages.success(request,f'Account created for {username} ')
-                return redirect('blog-home')
+                return redirect('login')
         else:
               form = UserRegistartionForm()
         return render(request, 'users/register.html', {'form': form})
+@login_required
+def profile(request):
+    return render(request,'users/profile.html')
